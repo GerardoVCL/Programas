2a657ea7cbd7c084abbe635497ee629725035a87
Gerardo Veliz <elgerardoveliz@gmail.com>
2022-01-02 15:50:21 -0300

    2do commit

--- mi-app/src/App.js
+++ mi-app/src/App.js
@@ -1,27 +1,34 @@
-import Button from './Button'
+import {Component} from 'react'
 
-//e es lo mismo que evento
-
-const arr = [
-    'chanchito feliz',
-    'chanchito triste',
-    'chanchito emocionado',
-]
-
-const App = () => {
-    const miVariable = false
-    if (miVariable){
-        return <p> Mi variable dio true</p>
-    }
-    return(
-        <div>
-            <h1 onClick={(e) => console.log('click', e)}>Hola mundo</h1>
-            {arr.map ( el => <p key={el}>{el}</p>)}
-            <Button onClick = {() => console.log ('clickeado')}>
+class Button extends Component{
+    render(){
+        console.log('ejecutando metodo render de button')
+        return(
+            <button>
                 Enviar
-            </Button>
-        </div>
-    )
+            </button>
+        )
+    }
+}
+class App extends Component {
+    state = {
+        valor : 3
+    }
+    render(){
+        console.log(this.state);
+        return(
+            <div>
+                <p>Hola mundo</p>
+                <Button />
+                <button 
+                className = {`${this.state.valor}`} 
+                onClick={() => this.setState({valor:2})}
+                >
+                    Enviar en App
+                </button>
+            </div>
+        )
+    }
 }
 
 export default App
