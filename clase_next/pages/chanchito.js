54e6bcf309edbfe543836037d0a80160218d30f3
Gerardo Veliz <elgerardoveliz@gmail.com>
2022-02-04 22:28:22 -0300

    clase de next.js y pokemones

---
+++ clase_next/pages/chanchito.js
@@ -0,0 +1,10 @@
+import Link  from 'next/link'
+const Chanchito = () => {
+    return(
+        <div>
+        <p>Chanchito Triste</p>
+        <Link href='/'>Inicio</Link>
+        </div>
+    )
+}
+export default Chanchito
